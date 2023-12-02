5dbac81c4e96c9225215e960c8440a0d060cb5a1
Owais Ahmad <owaisahmad014@gmail.com>
2023-12-02 19:13:54 +0500

    ....

--- globalVariable.py
+++ globalVariable.py
@@ -1,5 +1,14 @@
-#global varable
+# #global varable
 x = "awsome"
 def myFunction():
   print("python is " + x)
 myFunction()
+
+
+#execute this block of code separately
+x = "awsome"
+def myFunction():
+  x = "great"
+  print("python is "+ x)
+myFunction()
+print("python is " + x)
