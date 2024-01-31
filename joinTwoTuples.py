c86e34d7971dde079aeb02c0582d2faf81e953d8
Owais Ahmad <owaisahmad014@gmail.com>
2024-01-31 10:48:24 -0800

    joining tuples

---
+++ joinTwoTuples.py
@@ -0,0 +1,12 @@
+#To join two or more tuples you can use the + operator:
+
+tuple1 = ("a", "b", "c")
+tuple2 = (1,2,3)
+joinTuple = tuple1 + tuple2
+print(joinTuple)
+
+# Multiply Tuples
+tuple1 = ("Ha")
+tuple2 = (10)
+joinTuple = tuple1 * tuple2
+print(joinTuple)
