c457e3c11b501b978b22d1d7bd1894105e23db84
Owais Ahmad <owaisahmad014@gmail.com>
2024-02-16 20:26:35 +0500

    self parameter

--- class.py
+++ class.py
@@ -60,3 +60,24 @@ class Person:
 
 p1 = Person("John", 36)
 p1.myfunc()
+
+
+# The self Parameter
+
+"""
+The self parameter is a reference to the current instance of the class, and is used to access variables that belongs to the class.
+
+It does not have to be named self , you can call it whatever you like, but it has to be the first parameter of any function in the class:
+"""
+
+class Person:
+   def __init__(myself, name, age):
+      myself.name = name
+      myself.age = age
+
+   def myfunc(abc):
+      print("Hello i am your frient " + abc.name)
+
+p1 = Person("Owais khan", 24)
+p1.myfunc()
+      
