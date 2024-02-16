9336f416ba0d95af236dadec0034a2ad3608adc4
Owais Ahmad <owaisahmad014@gmail.com>
2024-02-16 20:19:56 +0500

    methods

--- class.py
+++ class.py
@@ -44,4 +44,19 @@ print(p1)
 
 # Object Methods
 
-            
+"""
+Objects can also contain methods. Methods in objects are functions that belong to the object.
+
+Let us create a method in the Person class:
+"""      
+
+class Person:
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+  def myfunc(self):
+    print("Hello my name is " + self.name)
+
+p1 = Person("John", 36)
+p1.myfunc()
