fb9f9d2b39ab39004e635e718fe8651f422ab4c5
Owais Ahmad <owaisahmad014@gmail.com>
2024-02-16 20:11:22 +0500

    str function

--- class.py
+++ class.py
@@ -20,4 +20,28 @@ class person:
 p1 = person('owais', 24)
 print(p1.name)
 print(p1.age)
-        
+
+
+# The __str__() Function
+
+"""
+The __str__() function controls what should be returned when the class object is represented as a string.
+
+If the __str__() function is not set, the string representation of the object is returned:
+"""
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def __str__(self):
+        return(f"{self.name}({self.age})")
+
+p1 = Person("owaiss", 33)
+print(p1)
+
+
+# Object Methods
+
+            
