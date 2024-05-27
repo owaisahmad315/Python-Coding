87fc5ede0202987721357418bfd3c0d062e5b70e
Owais Ahmad <owaisahmad014@gmail.com>
2024-05-27 14:50:36 +0500

    abs

---
+++ Absolute Value.py
@@ -0,0 +1,11 @@
+'''
+The built-in function abs() will return the absolute value of a number
+that you pass to it.
+'''
+
+miles_from_origin = 58
+miles_travelled = 93
+
+mile_to_go = miles_from_origin - miles_travelled
+print(mile_to_go)
+print(abs(mile_to_go))
